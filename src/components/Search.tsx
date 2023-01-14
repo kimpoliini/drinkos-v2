f8c7bfa9d15a407884f45934fd0fc31aa739b98e
= <=>
2023-01-14 18:20:31 +0100

    Added API call to api.ts, added search functionality to search bar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,17 +1,63 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import './search.css'
 import { ReactImageTint } from 'react-image-tint';
+import { IDrinkListItem } from './DrinkListItem';
+import { baseUrl, getDrinksFromUrl } from '../config/api';
+import { apiKey } from '../config/apiKey';
 
 const Search: FC = () => {
-  return (
-    <div className='search-bar'>
-      <input placeholder='Search drinks...' />
-      <div
-        onClick={() => { console.log("search") }}>
 
-        <ReactImageTint src={require('../assets/icons/search.png')} color="#a8b0c0" />
+  const [drinks, setDrinks] = useState<IDrinkListItem[]>([])
+
+  useEffect(() => {
+    let app = document.querySelector(".App")
+    let searchResults = document.querySelector(".search-results")
+    
+    window.addEventListener("resize", (e) => {
+      let width = window.innerWidth
+
+      let left = window.getComputedStyle(app!).marginLeft
+      let right = window.getComputedStyle(app!).marginRight
+
+
+      if (width >= 768) { //Tablet and bigger
+        searchResults!.setAttribute("style", `right: ${right}`)
+      } else if (width >= 500 && width < 768) { //Between tablet and Mobile
+        searchResults!.setAttribute("style", `left: ${left}; right: ${right}`)
+      } else if (width < 500) { //Mobile
+        searchResults!.removeAttribute("style")
+      }
+    })
+
+  }, [])
+
+  return (
+    <div>
+      <div className='search-bar'>
+        <input placeholder='Search drinks...'
+          onChange={(e) => {
+            let query = e.target.value
+            if (query.length >= 3) {
+              let url = `${baseUrl + apiKey}/search.php?s=${query}`
+              getDrinksFromUrl(url).then(drinks => {
+                setDrinks(drinks)
+              })
+            }
+
+          }} />
+        <div
+          onClick={() => { console.log("search") }}>
+
+          <ReactImageTint src={require('../assets/icons/search.png')} color="#a8b0c0" />
+        </div>
+      </div>
+      <div className="search-results">
+        {drinks ? (drinks.map((e: IDrinkListItem, i) => {
+          return <p key={i}>{e.name}</p>
+        })) : null}
       </div>
     </div>
+
   )
 }
 
--- src/config/api.ts
+++ src/config/api.ts
@@ -1,1 +1,39 @@
+import axios from "axios"
+import { IDrinkListItem } from "../components/DrinkListItem"
+
 export const baseUrl = "https://www.thecocktaildb.com/api/json/v2/"
+export const getDrinksFromUrl = async (url: string) => {
+    const promise = new Promise<IDrinkListItem[]>( (resolve, reject) => {
+        resolve( axios.get(url)
+            .then(resp => {
+                let drinks: IDrinkListItem[] = resp.data.drinks.map((e: any) => {
+
+                    let ingredients: string[] = []
+                    let measurements: string[] = []
+
+                    for (let i = 0; i < 15; i++) {
+                        let ingredient = e[`strIngredient${i + 1}`]
+                        let measurement = e[`strMeasure${i + 1}`]
+
+                        if (!ingredient && !measurement) break
+
+                        if (ingredient) ingredients.push(ingredient)
+
+                        if (measurement) measurements.push(measurement)
+                    }
+
+                    return {
+                        id: e.idDrink,
+                        name: e.strDrink,
+                        thumbnail: e.strDrinkThumb,
+                        ingredients: ingredients,
+                        measurements: measurements,
+                    }
+                })
+
+                return drinks
+            })
+        )
+    })
+    return promise
+}
