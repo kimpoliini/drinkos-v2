05383edad86959de2dedfbab5dd1039bc402371b
= <=>
2023-01-25 02:09:29 +0100

    Added functionality to page indicator

--- src/components/DrinkGrid.tsx
+++ src/components/DrinkGrid.tsx
@@ -17,6 +17,10 @@ const DrinkGrid: FC<IDrinkGrid> = (props) => {
     const [currentPage, setCurrentPage] = useState<number>(0)
 
     useEffect(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    }, [currentPage])
+
+    useEffect(() => {
         getDrinksFromUrl(props.url).then(drinks => {
             let perPage = 25
             let count = drinks.length
@@ -48,22 +52,29 @@ const DrinkGrid: FC<IDrinkGrid> = (props) => {
         })
     }, [props.url])
 
+    const decPage = () => { if (currentPage > 0) setCurrentPage(currentPage - 1) }
+    const incPage = () => { if (currentPage < pages.length - 1) setCurrentPage(currentPage + 1) }
+
     return (
         <div>
             <TextLine text={props.title} style={{ fontWeight: "normal" }} color="#404653" lineColor="#a8b0c0" />
 
             <div className='drink-grid'>
-                {pages[0].length > 0 ? pages[0].map((e: IDrinkListItem) => {
+                {pages[currentPage].length > 0 ? pages[currentPage].map((e: IDrinkListItem) => {
                     return <DrinkListItem key={e.id} id={e.id}
                         name={e.name} thumbnail={e.thumbnail} ingredients={e.ingredients} measurements={e.measurements} />
                 }) : (hasResults ? null : <p>No results</p>)}
             </div>
             <div className='page-indicator'>
-                <span className="material-icons">
+                <span className={`material-icons ${currentPage === 0 ? "indicator-disabled" : ""}`}
+                    onClick={() => decPage()}>
                     keyboard_arrow_left
                 </span>
-                {[...Array(pages.length)].map((e, i) => <span>{i + 1}</span>)}
-                <span className="material-icons">
+                {[...Array(pages.length)].map((e, i) => (
+                    <span className={currentPage === i ? "current-page" : ""}>{i + 1}</span>
+                ))}
+                <span className={`material-icons ${currentPage === pages.length - 1 ? "indicator-disabled" : ""}`}
+                    onClick={() => incPage()}>
                     keyboard_arrow_right
                 </span>
             </div>
