7451e7bc4dffbce4b8cafd03bc174e1b9dec3069
= <=>
2023-01-25 01:07:56 +0100

    Imported Material Icons, added page indicator

--- src/components/DrinkGrid.tsx
+++ src/components/DrinkGrid.tsx
@@ -1,8 +1,9 @@
 import React, { FC, useEffect, useState } from 'react'
-import { baseUrl, getDrinksFromUrl } from '../config/api'
+import { getDrinksFromUrl } from '../config/api'
 import DrinkListItem, { IDrinkListItem } from './DrinkListItem'
 import TextLine from './TextLine'
 import './drinkGrid.css'
+import { Link } from 'react-router-dom'
 
 export interface IDrinkGrid {
     title: string,
@@ -10,15 +11,38 @@ export interface IDrinkGrid {
 }
 
 const DrinkGrid: FC<IDrinkGrid> = (props) => {
-    const [drinks, setDrinks] = useState<IDrinkListItem[]>([])
+    // const [drinks, setDrinks] = useState<IDrinkListItem[]>([])
     const [hasResults, setHasResults] = useState<boolean>(true)
+    const [pages, setPages] = useState<[IDrinkListItem[]]>([[]])
+    const [currentPage, setCurrentPage] = useState<number>(0)
 
     useEffect(() => {
         getDrinksFromUrl(props.url).then(drinks => {
-            if (drinks.length > 0) {
-                setDrinks(drinks)
+            let perPage = 25
+            let count = drinks.length
+
+            if (count > 0) {
+                let pages = Math.floor(count / perPage) + 1
+
+                let newPages: [IDrinkListItem[]] = [[]]
+
+                console.log(count + " total");
+
+                for (let i = 0; i < pages; i++) {
+                    if (i + 1 === pages) {
+                        newPages[i] = drinks.slice(i * perPage, i * perPage + (count % perPage))
+                    } else {
+                        newPages[i] = drinks.slice(i * perPage, (i + 1) * perPage)
+                    }
+                }
+
+                console.log(newPages);
+
+                // setDrinks(drinks)
+                setPages(newPages)
             } else {
-                setDrinks([])
+                // setDrinks([])
+                setPages([[]])
                 setHasResults(false)
             }
         })
@@ -29,10 +53,19 @@ const DrinkGrid: FC<IDrinkGrid> = (props) => {
             <TextLine text={props.title} style={{ fontWeight: "normal" }} color="#404653" lineColor="#a8b0c0" />
 
             <div className='drink-grid'>
-                {drinks.length > 0 ? drinks.map((e: IDrinkListItem) => {
+                {pages[0].length > 0 ? pages[0].map((e: IDrinkListItem) => {
                     return <DrinkListItem key={e.id} id={e.id}
                         name={e.name} thumbnail={e.thumbnail} ingredients={e.ingredients} measurements={e.measurements} />
                 }) : (hasResults ? null : <p>No results</p>)}
+            </div>
+            <div className='page-indicator'>
+                <span className="material-icons">
+                    keyboard_arrow_left
+                </span>
+                {[...Array(pages.length)].map((e, i) => <span>{i + 1}</span>)}
+                <span className="material-icons">
+                    keyboard_arrow_right
+                </span>
             </div>
         </div>)
 }
