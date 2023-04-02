75412e27068f11846a2ac8e795fb5679c7ed90cc
Kim <kimpas@hotmail.se>
2023-04-03 01:15:32 +0200

    Added icons for Contact, added content to Contact, created component

--- src/pages/footer/About.tsx
+++ src/pages/footer/About.tsx
@@ -7,7 +7,8 @@ const About = () => {
         <div>
             <h2>About Drinkos</h2>
             <h3>History</h3>
-            <p>Drinkos started out as a school project
+            <p>
+                Drinkos started out as a school project
                 (see <a href="https://kimpoliini.github.io/drinkos-legacy/">drinkos-legacy</a>)
                 in 2020. I decided after my studies that I wanted to create a full website from that first draft,
                 and set out to create this new version of Drinkos.
@@ -24,6 +25,14 @@ const About = () => {
                 If there is something you would like to see changed or improved,
                 feel free to <Link to="/suggestchange">suggest a change.</Link>
             </p>
+            {/* <h3>Measurement units</h3>
+            <p>
+                The drinks on this site are originally measured in imperial units, which can be
+                a hassle to convert manually. So to make this site accesible
+                and easy-to-use, I added the ability to easily convert
+                from imperial to metric units, saving you, the aspiring drink-mixer,
+                from the trouble. See the <Link to={"/unitconversions"}>conversion table</Link> for more information.
+            </p> */}
             <h3>Donation</h3>
             <p id='donation'>
                 This website is made entirely on my free time, and if you would like
--- src/pages/footer/Contact.tsx
+++ src/pages/footer/Contact.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import IconWithText from '../../components/IconWithText'
+import "./footerPage.css"
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <h2>Contact</h2>
+      <p>
+        Feel free to contact me if you would like to work together, or have any
+        other type of inquiry. If you have suggestions or questions about the
+        site itself, please consider <Link to={"/suggestchange"}>suggesting a change</Link>.
+      </p>
+
+      <IconWithText
+        src={require("../../assets/icons/email.png")}
+        text='Email - kim.hman@hotmail.com' />
+
+      <IconWithText
+        src={require("../../assets/icons/github.png")}
+        link="https://github.com/kimpoliini"
+        text='GitHub - kimpoliini' />
+        
+      <IconWithText
+        src={require("../../assets/icons/website.png")}
+        link="https://kimpoliini.github.io/portfolio/"
+        text='Personal website/portfolio' />
+    </div>
   )
 }
 
