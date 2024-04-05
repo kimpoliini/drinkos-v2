9644905d0b2457295299c121bf8a3419c9a57fd7
kim <kimpas@hotmail.se>
2024-04-05 02:11:32 +0200

    Added hamburger menu to header, moved link to random drink to header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 import './App.css';
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 import SearchBar from './components/SearchBar';
 import { ScrollContext } from './config/ScrollContext';
 
 const App: FC = () => {
   const [value, setValue] = useState<number>(0)
+  const menuButton = useRef<HTMLDivElement>(null)
+  const [isMenuHidden, setIsMenuHidden] = useState<boolean>(true)
+  const location = useLocation();
+
+  //Hide menu button if it's open when the URL changes
+  useEffect(() => {
+    if (!isMenuHidden) {
+      menuButton.current?.classList.toggle("menu-hidden")
+      setIsMenuHidden(true)
+    }
+  }, [location])
 
   return (
     <div className='app-wrapper'>
@@ -13,11 +24,27 @@ const App: FC = () => {
         <header>
           <Link to={'/'}><h1>Drinkos</h1></Link>
           <nav>
-            <Link to={'/'}>Home</Link>
-            <Link to={'/latest'}>Latest</Link>
-            <Link to={'/browse'}>Browse</Link>
+            <span onClick={() => {
+              menuButton.current?.classList.toggle("menu-hidden")
+              setIsMenuHidden(menuButton.current?.classList.contains("menu-hidden")!)
+            }}
+              className="material-icons menu-button">
+              {isMenuHidden ? "menu" : "close"}
+            </span>
+            <div className='header-links'>
+              <Link to={'/'}>Home</Link>
+              <Link to={'/latest'}>Latest</Link>
+              <Link to={'/browse'}>Browse</Link>
+              <Link to={'/randomdrink'}>Random drink</Link>
+            </div>
 
             <SearchBar />
+            <div ref={menuButton} className='menu-links menu-hidden'>
+              <Link to={'/'}>Home</Link>
+              <Link to={'/latest'}>Latest</Link>
+              <Link to={'/browse'}>Browse</Link>
+              <Link to={'/randomdrink'}>Random drink</Link>
+            </div>
           </nav>
         </header>
 
@@ -34,7 +61,6 @@ const App: FC = () => {
           <Link to={'/contact'}>Contact</Link>
           <Link to={'/suggestchange'}>Suggest a change</Link>
 
-          <p>Take me to a <Link to={'/randomdrink'}>random drink</Link></p>
           <p>Results provided by <a href="https://www.thecocktaildb.com/" rel='nofollow'>TheCocktailDB</a></p>
         </div>
       </footer>
